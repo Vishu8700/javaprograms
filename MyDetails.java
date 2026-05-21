1d53e072914bfca0f713fb7ef5528c82d4d9b998
Vishu8700! <vg0836760@gmail.com>
2026-05-21 08:52:12 +0530

    my details

---
+++ MyDetails.java
@@ -0,0 +1,9 @@
+package basicprograms;
+
+public class MyDetails {
+    public static void main(String[] args) {
+        System.out.println("My Nmae is VISHAL");
+        System.out.println("I am big fan of MSD");
+        System.out.println("I am also a good Cricater");
+    }
+}
