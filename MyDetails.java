b64dd5b32644ac218e97190e88c68c235207d696
Vishu8700! <vg0836760@gmail.com>
2026-05-21 11:56:16 +0530

    second commit

--- MyDetails.java
+++ MyDetails.java
@@ -3,7 +3,10 @@ package basicprograms;
 public class MyDetails {
     public static void main(String[] args) {
         System.out.println("My Nmae is VISHAL");
-        System.out.println("I am big fan of MSD");
-        System.out.println("I am also a good Cricater");
+        System.out.println("I am live in Kavinagar Ghaziabad");
+        System.out.println("My motherland is Gorakhpur");
+        System.out.println("in my family we are 5 members mom & dad and two sisters");
+        System.out.println("Now i am pursuing BCA from sunder deep group of inst");
+        System.out.println("i am Good programmer and also self motiveted person");
     }
 }
